671309205e916509334e27ccc585932431fc01e3
LedezmaG <israeledezmag@gmail.com>
2020-01-19 18:01:43 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = (
+    <div>
+
+        <h1>Hi!! I'm David</h1>
+        <p>I'm a web developer</p>
+
+    </div>
+)
+
+const conteiner = document.getElementById('app');
+ReactDOM.render(jsx, conteiner);
