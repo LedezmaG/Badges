8da0091ae7a5d161f9c791f7a0dbc919511c32c2
Ledezmag <israeledezma@gmail.com>
2020-03-03 18:29:48 -0600

    call to api (we have a problem :(  )

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
-
-
 import App from './component/App.js'
+import BadgeNew from './pages/BadgeNew'
+import Badge from './pages/Badges'
 
 const conteiner = document.getElementById('app');
 
-ReactDOM.render(<BadgeNew/>, conteiner);
+ReactDOM.render(<Badge/>, conteiner);
 
